7723dd12b974785b8063980ff4f4663e4f778c99
telegramcosca <telegramclonemaking@gmail.com>
2026-09-20 18:18:46 +0530

    Update Google OAuth callback URL for production

--- index.js
+++ index.js
@@ -45,8 +45,9 @@ passport.deserializeUser(async (id, done) => {
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "/auth/google/callback"
+    callbackURL: "https://cosca-production-ec29.up.railway.app/auth/google/callback"
   },
+                                
   async (accessToken, refreshToken, profile, done) => {
     try {
       // Check karo agar user pehle se hai

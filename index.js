cc66fff5fc781f2fc5dc6335cf859b1ae322bb55
telegramcosca <telegramclonemaking@gmail.com>
2026-09-21 09:16:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -122,15 +122,26 @@ app.get('/auth/google/callback',
 );
 
 // ==========================================
-// GITHUB AUTH ROUTES
+// GOOGLE AUTH ROUTES
 // ==========================================
-// 5. Authentication Routes (Login URLs)
 app.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
 
 app.get('/auth/google/callback', 
   passport.authenticate('google', { failureRedirect: '/' }),
   (req, res) => {
-    res.redirect('/'); // Login hone ke baad chat par wapas bhej do
+    res.redirect('/'); 
+  }
+);
+
+// ==========================================
+// GITHUB AUTH ROUTES
+// ==========================================
+app.get('/auth/github', passport.authenticate('github', { scope: [ 'user:email' ] }));
+
+app.get('/auth/github/callback', 
+  passport.authenticate('github', { failureRedirect: '/' }),
+  (req, res) => {
+    res.redirect('/'); 
   }
 );
 
